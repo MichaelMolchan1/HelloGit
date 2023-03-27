770bd101820402eb6e355a8cb09e0e5c886635a6
Molch <molchaml@mail.uc.edu>
2023-03-27 11:40:09 -0400

    Add project files.

---
+++ HelloGit/HelloGit.cpp
@@ -0,0 +1,14 @@
+/*
+Michael Molchan
+3/27/2023
+
+A program that prints out "Hello, World!" that is being used as an example of how to use Github
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\n";
+    return 0;
+}
